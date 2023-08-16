6b78352c8d641a89b0b3e31013b3272f1fced505
unknown <sundawarifky@gmail.com>
2023-08-16 16:30:52 +0700

    Update UI V3.7 Added Edit + Delete so its a CRUD now

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -203,11 +203,118 @@
         #select-angkatan {
             border: 2px solid var(--input-focus);
         }
+        .buttonback {
+            width: 140px;
+            height: 56px;
+            overflow: hidden;
+            border: none;
+            margin-left: 90%;
+            color: #000000;
+            background: none;
+            position: relative;
+            padding-bottom: 2em;
+        }
+
+        .buttonback>div,
+        .buttonback>svg {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            display: flex;
+        }
+
+        .buttonback:before {
+            content: "";
+            position: absolute;
+            height: 2px;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            transform: scaleX(0);
+            transform-origin: bottom right;
+            background: currentColor;
+            transition: transform 0.25s ease-out;
+        }
+
+        .buttonback:hover:before {
+            transform: scaleX(1);
+            transform-origin: bottom left;
+        }
+
+        .buttonback .clone>*,
+        .buttonback .text>* {
+            opacity: 1;
+            font-size: 1.3rem;
+            color: 
+            transition: 0.2s;
+            margin-left: 4px;
+        }
+
+        .buttonback .clone>* {
+            transform: translateY(60px);
+        }
+
+        .buttonback:hover .clone>* {
+            opacity: 1;
+            transform: translateY(0px);
+            transition: all 0.2s cubic-bezier(0.215, 0.61, 0.355, 1) 0s;
+        }
+
+        .buttonback:hover .text>* {
+            opacity: 1;
+            transform: translateY(-60px);
+            transition: all 0.2s cubic-bezier(0.215, 0.61, 0.355, 1) 0s;
+        }
+
+        .buttonback:hover .clone> :nth-child(1) {
+            transition-delay: 0.15s;
+        }
+
+        .buttonback:hover .clone> :nth-child(2) {
+            transition-delay: 0.2s;
+        }
+
+        .buttonback:hover .clone> :nth-child(3) {
+            transition-delay: 0.25s;
+        }
+
+        .buttonback:hover .clone> :nth-child(4) {
+            transition-delay: 0.3s;
+        }
+
+        /* icon style and hover */
+        .buttonback svg {
+            width: 20px;
+            right: 0;
+            top: 50%;
+            transform: translateY(-50%) rotate(-50deg);
+            transition: 0.2s ease-out;
+        }
+
+        .buttonback:hover svg {
+            transform: translateY(-50%) rotate(-90deg);
+        }
     </style>
 @endsection
 
 @section('content')
     <div class="container mt-4">
+        <button class="buttonback" onclick="DataHome()">
+            <div class="text">
+                <span><strong>Back</strong></span>
+                <span><strong>to</strong></span>
+                <span><strong>top</strong></span>
+            </div>
+            <div class="clone">
+                <span><strong>Back</strong></span>
+                <span><strong>to</strong></span>
+                <span><strong>top</strong></span>
+            </div>
+            <svg width="20px" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+            </svg>
+        </button>
         <div class="outer-box">
             <button data-text="Awesome" class="button" style="margin-left: 2%">
                 <span class="actual-text">&nbsp;edit&nbsp;</span>
@@ -288,12 +395,8 @@
         tinymce.init({
             selector: '#mytextarea_pertanyaan, #mytextarea_jawaban'
         });
-    </script>
-
-    <style>
-        .custom-dropify {
-            width: 100%;
-            height: 100%;
+        function DataHome() {
+            window.location.href = "{{ route('dashboard') }}"
         }
-    </style>
+    </script>
 @endsection
